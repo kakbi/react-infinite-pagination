8736382e0c384d7c0ab0327876ca4a4423225a85
Vadym Chebotar <dramvoda@gmail.com>
2026-01-14 19:20:12 +0200

    feat: complete infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,32 @@ function App() {
     const [photos, setPhotos] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [fetching, setFetching] = useState(true);
+    const [totalCount, setTotalCount] = useState(0);
 
     useEffect(() => {
         if (fetching) {
+            console.log('fetching');
             axios
                 .get(
-                    'https://jsonplaceholder.typicode.com/photos?_limit=10&_page=1'
+                    `https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`
                 )
-                .then((response) => setPhotos(response.data));
+                .then((response) => {
+                    setPhotos([...photos, ...response.data]);
+                    setCurrentPage((prevState) => prevState + 1);
+                    setTotalCount(response.headers['x-total-count']);
+                })
+                .finally(() => setFetching(false));
         }
     }, [fetching]);
 
     const scrollHandler = (e) => {
-        console.log('scroll');
+        if (
+            e.target.documentElement.scrollHeight -
+                (e.target.documentElement.scrollTop + window.innerHeight) <
+                100 &&
+            photos.length >= totalCount
+        )
+            setFetching(true);
     };
 
     useEffect(() => {
@@ -48,8 +61,6 @@ function App() {
                     />
                 </div>
             ))}
-
-            <div className="loading-marker">Loading new cards...</div>
         </div>
     );
 }
